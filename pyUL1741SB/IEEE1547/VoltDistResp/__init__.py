8c93e09f9ca0be6da9660072b78b715c9bab809a
kxie <kxie@ualberta.ca>
2025-09-07 15:00:54 -0600

    update vrt doc comments, lvrt prog

--- pyUL1741SB/IEEE1547/VoltDistResp/__init__.py
+++ pyUL1741SB/IEEE1547/VoltDistResp/__init__.py
@@ -1,10 +1,18 @@
 '''
 5.4 Test for response to voltage disturbances
 '''
+from enum import Enum
 
+import numpy as np
+import pandas as pd
+import enum
+
+from pyUL1741SB.IEEE1547.VoltReg.vw import VWCurve
+from pyUL1741SB.IEEE1547.VoltReg.vv import VVCurve
+from pyUL1741SB.IEEE1547.VoltReg.wv import WVCurve
+from pyUL1741SB.IEEE1547.FreqSupp import FW_OF, FW_UF
 from pyUL1741SB.eut import Eut
 from pyUL1741SB.env import Env
-import numpy as np
 
 class VoltShallTripValue:
     def __init__(self, volt_pu, cts, volt_pu_min=None, volt_pu_max=None, cts_min=None, cts_max=None):
@@ -85,36 +93,73 @@ class VoltShallTripTable:
             VoltShallTripValue(0.50, 2.0, 0.0, 0.50, 2.0, 21.0)    # UV2 (different values)
         )
 
+class OpMode(enum.Enum):
+    ContOp = 1
+    MandOp = 2
+    MandOpC = 3  # During this test condition, the EUT may stay in momentary cessation for voltages less than 0.5 p.u. but shall not trip
+    MomCess = 4
+
+
 class LVRTSeq:
-    def AOPCatI(self):
+    def __init__(self, df_dset1, df_dset2, df_dset3, df_dset4):
+        self.df_dsets = [df_dset1, df_dset2, df_dset3, df_dset4]
+    @staticmethod
+    def AOPCatI():
         raise NotImplementedError
-    def AOPCatII(self):
+    @staticmethod
+    def AOPCatII():
         raise NotImplementedError
-    def AOPCatIII(self):
-        # 0.88-1.00 for 5s
-        # 0.00-0.05 for 1s
-        # 0.00-0.50 for 9s
-        # 0.50-0.70 for 10s
-
-        # 0.88-1.00 for 5s
-        # 0.00-0.05 for 1s
-        # 0.00-0.50 for 9s
-        # 0.50-0.70 for 10s
-
-        # 0.88-1.00 for 5s
-        # 0.00-0.05 for 1s
-        # 0.00-0.50 for 9s
-        # 0.50-0.70 for 10s
-        # 0.88-1.00 for 120s
-
-        # required if
-        # 0.88-1.00 for 5s
-        # 0.00-0.05 for 1s
-        # 0.52-0.70 for 19s
-        # 0.88-1.00 for 120s
-        pass
+    @staticmethod
+    def AOPCatIII():
+        # 0.88-1.00 for 5s, OpMode.ContOp
+        # 0.00-0.05 for 1s, OpMode.MomCess
+        # 0.00-0.50 for 9s, OpMode.MandOp
+        # 0.50-0.70 for 10s, OpMode.MandOp
+        df_dset1 = pd.DataFrame({
+            VoltDist.Vminkey: [0.88, 0.00, 0.00, 0.50],
+            VoltDist.Vmaxkey: [1.00, 0.05, 0.50, 0.70],
+            VoltDist.Durkey: [5, 1, 9, 10],
+            VoltDist.OpMdkey: [OpMode.ContOp, OpMode.MomCess, OpMode.MandOpC, OpMode.MandOp]
+        })
+        # 0.88-1.00 for 5s, OpMode.ContOp
+        # 0.00-0.05 for 1s, OpMode.MomCess
+        # 0.00-0.50 for 9s, OpMode.MandOp
+        # 0.50-0.70 for 10s, OpMode.MandOp
+        df_dset2 = pd.DataFrame({
+            VoltDist.Vminkey: [0.88, 0.00, 0.00, 0.50],
+            VoltDist.Vmaxkey: [1.00, 0.05, 0.50, 0.70],
+            VoltDist.Durkey: [5, 1, 9, 10],
+            VoltDist.OpMdkey: [OpMode.ContOp, OpMode.MomCess, OpMode.MandOpC, OpMode.MandOp]
+        })
+        # 0.88-1.00 for 5s, OpMode.ContOp
+        # 0.00-0.05 for 1s, OpMode.MomCess
+        # 0.00-0.50 for 9s, OpMode.MandOp
+        # 0.50-0.70 for 10s, OpMode.MandOp
+        # 0.88-1.00 for 120s, OpMode.ContOp
+        df_dset3 = pd.DataFrame({
+            VoltDist.Vminkey: [0.88, 0.00, 0.00, 0.50, 0.88],
+            VoltDist.Vmaxkey: [1.00, 0.05, 0.50, 0.70, 1.00],
+            VoltDist.Durkey: [5, 1, 9, 10, 120],
+            VoltDist.OpMdkey: [OpMode.ContOp, OpMode.MomCess, OpMode.MandOpC, OpMode.MandOp, OpMode.ContOp]
+        })
+        # required if restore output capability could not be evaluated in other sequences
+        # 0.88-1.00 for 5s, OpMode.ContOp
+        # 0.00-0.05 for 1s, OpMode.MomCess
+        # 0.52-0.70 for 19s, OpMode.MandOp
+        # 0.88-1.00 for 120s, OpMode.ContOp
+        df_dset4 = pd.DataFrame({
+            VoltDist.Vminkey : [0.88, 0.00, 0.52, 0.88],
+            VoltDist.Vmaxkey : [1.00, 0.05, 0.70, 1.00],
+            VoltDist.Durkey : [5, 1, 19, 120],
+            VoltDist.OpMdkey : [OpMode.ContOp, OpMode.MomCess, OpMode.MandOp, OpMode.ContOp]  # third step is used to eval restore output performance
+        })
+        return LVRTSeq(df_dset1, df_dset2, df_dset3, df_dset4)
 
 class VoltDist:
+    Vminkey = 'Vmin'
+    Vmaxkey = 'Vmax'
+    Durkey = 'dur_s'
+    OpMdkey = 'OpMd'
     def ov_trip_proc(self, env: Env, eut: Eut):
         """"""
         multiphase = eut.multiphase
@@ -285,24 +330,75 @@ class VoltDist:
         multiphase = eut.multiphase
         if multiphase:
             raise NotImplementedError
+
+        if eut.Cat == Eut.Category.A:
+            vvcrv = VVCurve.Crv_1A(eut.Prated, eut.VN)
+        elif eut.Cat == Eut.Category.B:
+            vvcrv = VVCurve.Crv_1B(eut.Prated, eut.VN)
+        else:
+            raise TypeError(f'unknown category {eut.Cat}')
+
+        if eut.aopCat == Eut.AOPCat.I:
+            lvrt_seq = LVRTSeq.AOPCatI()
+            fw_of = FW_OF.CatI_CharI()
+            fw_uf = FW_UF.CatI_CharI()
+        elif eut.aopCat == Eut.AOPCat.II:
+            lvrt_seq = LVRTSeq.AOPCatII()
+            fw_of = FW_OF.CatII_CharI()
+            fw_uf = FW_UF.CatII_CharI()
+        elif eut.aopCat == Eut.AOPCat.III:
+            lvrt_seq = LVRTSeq.AOPCatIII()
+            fw_of = FW_OF.CatIII_CharI()
+            fw_uf = FW_UF.CatIII_CharI()
+        else:
+            raise TypeError(f'unknown category {eut.aopCat}')
         '''
         The settings for magnitude and duration of undervoltage tripping functions shall be
         disabled or set so as not to influence the outcome of the test. 
-        '''
-        '''
         The voltage-reactive power control mode of the EUT shall be set to the default settings specified in Table 8 
         of IEEE Std 1547-2018 for the applicable performance category, and enabled. 
-        '''
-        '''
         If the EUT provides a voltage-active power control mode, that mode shall be disabled. 
-        '''
-        '''
         The frequency-active power control mode of the EUT shall be set to the default settings.
         '''
+        # disable uv trip, set vvcrv, disable vw, set vwcrv
         '''
         The ride-through tests shall be performed at two output power levels, high and low, and at any convenient
         power factor greater than 0.90. High power is more than 0.9 of rated, low is between 0.25 to 0.5 of rated
         '''
-        for PF in [1.0, 0.9]:
-            for pwr_pct in [1.0, 0.25]:
-                raise NotImplementedError
+        PF = 1.0  # 0.9-1.0
+        eut.fixed_pf(PF=PF)
+        for pwr_pct in [1.0, 0.25]:  # >0.9, 0.25-0.50
+            eut.active_power(WLimPct=pwr_pct*100)
+            cond = True
+            for df_set in lvrt_seq.df_dsets[:-1]:  # iterate over necessary sets
+                self.uvrt_validate(env, eut, df_set)
+                cond = True  # TODO required if restore output capability could not be evaluated in other sequences
+            if cond:
+                df_set = lvrt_seq.df_dsets[-1]
+                self.uvrt_validate(env, eut, df_set)
+
+    def uvrt_validate(self, env: Env, eut: Eut, df_set):
+        """"""
+        '''
+        Where the operating mode is specified as Mandatory Operation, the EUT shall not trip, shall
+        maintain synchronism and maintain its total apparent current during the disturbance period at or
+        above 80% of the pre-disturbance value.
+        
+        Where the operating mode is specified as Permissive Operation, the EUT may Cease to Energize
+        followed by Restore Output, or may continue to exchange current with the ac test source. The EUT
+        shall ride through and shall not trip during Permissive Operation. Where the EUT rides through in
+        a Cease to Energize state, the EUT shall comply with the Restore Output requirements in 6.4.2.7 of
+        IEEE Std 1547-2018.
+        
+        Where the operating mode is specified as Momentary Cessation, the EUT shall cease to energize
+        the ac test source. Following the momentary cessation event, the EUT shall comply with the
+        Restore Output requirements of 6.4.2.7 of IEEE Std 1547-2018.
+        '''
+        for row in df_set.iterrows():
+            vpu = (row[self.Vminkey] + row[self.Vmaxkey]) / 2
+            dur = row[self.Durkey]
+            opmd = row[self.OpMdkey]
+            env.ac_config(Vac=vpu*eut.VN)
+            # wait dur duration
+            # eut behavior match opmd
+            raise NotImplementedError
